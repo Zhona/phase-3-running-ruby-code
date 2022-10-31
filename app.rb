7a686e7c807fb8d175fcb3df6d2529e754c1cb7d
Teana Zhong <Teanawk@gmail.com>
2022-10-31 12:39:12 -0400

    commit tree 17b6cdc7e1a09fe5fa8ab81bafb7e83ca855b319

---
+++ app.rb
@@ -0,0 +1,12 @@
+puts "Hello World!" # adds line breal
+puts "Hello World!"
+puts "Hello World!"
+
+print "Pass this test, please." # prints on the same line 
+print "Pass this test, please."
+print "Pass this test, please."
+
+p [1,2,3]
+puts [1,2,3] # prints each number on a new line 
+
+print [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
